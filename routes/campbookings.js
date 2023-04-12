88863445dd9d49ff1c9baa3bedf1bb65fbeb1c89
Chakin <chakin.pattanasombatsakul@gmail.com>
2023-04-12 22:10:05 +0700

    Bug fix

--- routes/campbookings.js
+++ routes/campbookings.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const {protect, authorize} = require('../middleware/auth');
 
 //Re-route into other resource routers
-router.use('/:campbookingId/appointment/', appointmentRouter);
+router.use('/:campbookingId/appointments/', appointmentRouter);
 
 router.route('/').get(getCampbookings).post(protect, authorize('admin'), createCampbooking);
 router.route('/:id').get(getCampbooking).put(protect, authorize('admin'), updateCampbooking).delete(protect, authorize('admin'), deleteCampbooking);
